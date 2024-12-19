604de0a6ab0842acccb4dc8cb2b58ede4748da33
Gajanan Rathod <97657400+gajanansr@users.noreply.github.com>
2024-12-19 20:55:07 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import logo from "../images/logo.png"
 import { Link, useNavigate } from 'react-router-dom'
-import Avatar from 'react-avatar';
+//import Avatar from 'react-avatar';
 import { MdLightMode } from "react-icons/md";
 import { BsGridFill } from "react-icons/bs";
 import { api_base_url, toggleClass } from '../helper';
