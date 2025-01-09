7334314aeb531a4add7aefc09baf4a2e023bc2c1
Gajanan Rathod <97657400+gajanansr@users.noreply.github.com>
2025-01-09 23:45:05 +0530

    databaseconfig

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,6 +1,6 @@
 let mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/codeIDE');
+mongoose.connect('mongodb+srv://gajanan1055:<KRlmgVnUwcCwJQFp>@bank.c9ts2.mongodb.net/?retryWrites=true&w=majority&appName=bank');
 
 let userSchema = new mongoose.Schema({
   name: String,
